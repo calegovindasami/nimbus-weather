1e48fcecaac7d4f44a013f35060097c86c11fa3a
Cale Govindasami <calegovindasami@gmail.com>
2025-03-27 22:38:27 +0200

    Added doxyfile and redid pipeline

--- weather.c
+++ weather.c
@@ -12,6 +12,13 @@ int main() {
 #endif
 
 
+/**
+ * @brief Returns the welcome message for the application
+ * 
+ * @return const char* A pointer to the first character of the string.
+ *         All characters end with a \0 which indicates its the end of the string.
+ *         'Hello' -> ["H", "e", "l", "l", "\0"]
+ */
 const char* getWelcomeMessage(void) {
     return "Welcome to the Nimbus Weather CLI!";
 }
